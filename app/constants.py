7cefc50386f6cbe95c9a3645d61c12d1b0210e82
Sneh Pushp <pushp1999satyam@gmail.com>
2025-06-10 14:56:30 +0530

    Updated config

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,4 @@
 import os
 
 ROOT_DIR = os.path.abspath(os.curdir)
-CONFIGMAP_PATH = os.getenv("configmap_path", f"{ROOT_DIR}/app/configs/.env")
+CONFIGMAP_PATH = os.getenv("configmap_path", f"{ROOT_DIR}/app/config/.env")
